b16d2629393b5ae290a491121e8e1c6b9abda13f
Camila Kunitz <camila.kunitz@gmail.com>
2022-03-02 15:51:38 -0300

    Cria Tooltip mapa

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ import initAnimacaoScroll from './js-modules/animacao-scroll.js';
 import initAccordion from './js-modules/accordion.js';
 import initTabNav from './js-modules/tabnav.js';
 import initModal from './js-modules/modal.js';
+import initTooltip from './js-modules/tooltip.js';
 
 initTabNav();
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initModal();
+initTooltip();
