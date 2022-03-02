c23cea3bc26b2de96aab9e68b339a2638cb42f4c
Camila Kunitz <camila.kunitz@gmail.com>
2022-03-02 11:48:13 -0300

    Cria modal

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ import initScrollSuave from './js-modules/scroll-suave.js';
 import initAnimacaoScroll from './js-modules/animacao-scroll.js';
 import initAccordion from './js-modules/accordion.js';
 import initTabNav from './js-modules/tabnav.js';
+import initModal from './js-modules/modal.js';
 
 initTabNav();
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
+initModal();
